ba07256c68f6ce87efe2c6581973d3d00acc9c61
Alexandr150983 <alexandr150983@gmail.com>
2023-09-24 18:56:29 +0300

    task 1 full

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Button } from './FeedbackOptions.styled';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
